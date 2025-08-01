83fcfb917acca254864f57e9ecdd7dfb23280b14
Gia Huy Le <huyyyylegia@gmail.com>
2025-08-01 16:02:40 +0700

    basic nextjs frontend

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+    return ( 
+        <div className='flex flex-col items-center justify-center min-h-screen'>
+            Login
+        </div>
+    );
+};
+
+export default LoginPage;
