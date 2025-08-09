43048fe02a4422fa79d5c15d8ec43aee5a7b7e20
Gia Huy Le <huyyyylegia@gmail.com>
2025-08-10 01:44:42 +0700

    finished routing for channels and chats

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -34,6 +34,7 @@ export const ServerMember = ({
         <button
             onClick={onClick}
             className={cn(
+                "cursor-pointer",
                 "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
                 params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
             )}
