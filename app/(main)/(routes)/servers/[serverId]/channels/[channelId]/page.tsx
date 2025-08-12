dbbd2f87c84f84600baa8b0831c0eff5d502a443
Gia Huy Le <huyyyylegia@gmail.com>
2025-08-12 10:00:15 +0700

    chat input added

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
@@ -44,6 +45,20 @@ const ChannelIdPage = async ({
                 name={channel.name}
                 serverId={channel.serverId}
                 type="channel"
+            />
+
+            <div className="flex-1">
+                hello future
+            </div>
+
+            <ChatInput 
+                name={channel.name}
+                type="channel"
+                apiUrl="/api/socket/messages"
+                query = {{
+                    channeldId: channel.id,
+                    serverId: channel.serverId
+                }}
             />
         </div>
     );
