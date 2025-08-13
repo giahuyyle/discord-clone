a99d6c613f22022247843cbf38c391b789faf2f0
Gia Huy Le <huyyyylegia@gmail.com>
2025-08-13 16:57:18 +0700

    update click on member functionality

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -135,7 +135,7 @@ export const ChatItem = ({
     return (
         <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
             <div className="group flex gap-x-2 items-start w-full">
-                <div className="cursor-pointer hover:drop-shadow-md transition">
+                <div onClick={onMemberClick} className="cursor-pointer hover:drop-shadow-md transition">
                     <UserAvatar src={member.profile.imageUrl} />
                 </div>
                 <div className="flex flex-col w-full">
@@ -143,6 +143,7 @@ export const ChatItem = ({
                         <div className="flex items-center">
                             <p
                                 className="font-semibold text-sm hover:underline cursor-pointer"
+                                onClick={onMemberClick}
                             >
                                 {member.profile.name}
                             </p>
